42ec01eb5a5e31ac2b4c091a1489186f95e116bb
IchiganCS <mail@ichigancs.com>
2022-06-10 00:54:26 +0200

    barebone artistlist

---
+++ Pages/ArtistList.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Technecon.Pages;
+
+public class ArtistListModel : PageModel {
+
+    public ArtistListModel() {
+    }
+
+    public void OnGet() {
+
+    }
+}
